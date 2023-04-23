a2166f2e425ca06c09ba546018db220a772c261e
Sergey N <nsergey82@gmail.com>
2023-04-23 20:03:24 +0100

    Add single worker

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask
+
+from datetime import datetime
+from threading import Thread
+from threading import Lock
+
+import time
+
+WORKER_IDLE_SECONDS = 600
+
+def worker():
+    i = 0
+    while True:
+        i += 1
+        print(f"worker active [{i}] {datetime.now()}")
+        time.sleep(WORKER_IDLE_SECONDS)
+
+
+w = Thread(target=worker, daemon=True)
+w.start()
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
