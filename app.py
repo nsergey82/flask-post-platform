6364653870c2406c9546d4a86fd15b1ad5d9f3f4
Sergey N <nsergey82@gmail.com>
2023-04-24 22:34:32 +0100

    Add basic oidc login flow
    So we can store tokens inside worker

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, request, session, redirect
 
 from datetime import datetime
 from threading import Thread
-
 import time
 
+from oidcutils import (
+    handle_callback,
+    init_oidc,
+    dpop_from_atoken_for_url,
+    prepare_auth_data,
+    webid_from_access_token
+)
+
+# URL when deployed to render, change to server
+_THIS = "https://rss-post-platform.onrender.com"
+# ID provider. Can support many
+_ISSUER = "https://solidcommunity.net/"
+# the route in this app used for handling idp redirects back
+_OID_CALLBACK_PATH = "/oauth/callback"
+_CALLBACK_URL = f"{_THIS}{_OID_CALLBACK_PATH}"
+_TEST_URL = "https://sergeynepomnyachiy.solidcommunity.net/private/test2.md"
+
 WORKER_IDLE_SECONDS = 30
-state = {}
+worker_state = {"users": {}}
+provider_info, client_id = init_oidc(_ISSUER, _CALLBACK_URL)
+
+# keyed by state, contains {'key': {...}, 'code_verifier': ...}
+# will hold sessions partially established
+# (e.g., one way sent, redirect didn't return)
+state_storage = {}
+
+
+def get_from_session_storage(key):
+    global state_storage
+    assert key in state_storage, f"key '{key}' not in STATE_STORAGE?"
+    return state_storage[key]
+
+
+def set_session_storage(key, value):
+    global state_storage
+    state_storage[key] = value
 
 
 def worker():
@@ -14,28 +47,76 @@ def worker():
     while True:
         i += 1
         now = datetime.now()
-        state["worker"] = i
-        state["latest"] = now
         print(f"Backend worker active [{i}] {now}")
+
+        worker_state["worker"] = i
+        worker_state["latest"] = now
+        print("Users:", worker_state["users"].keys())
+
         time.sleep(WORKER_IDLE_SECONDS)
 
 
-w = None
+worker_thread = None
 
 app = Flask(__name__)
+app.secret_key = "2612ea_678df63cda52e_fdff4e424e45_3"
+
 
 @app.route("/health")
 def health():
     return "OK"
 
+
+@app.route("/login")
+def login():
+    # we are fully logged in and have an access token
+    if ("access_token" in session):
+        msg = f"Logged in successfully as {webid_from_access_token(session['access_token'])}"
+        print(msg)
+        return msg
+
+    global client_id
+    key, value, query = prepare_auth_data(request.url, client_id, _CALLBACK_URL)
+    set_session_storage(key, value) # we need this partial session for when they come back
+
+    # send them to go to their id provider (e.g. solidcommunity)
+    url = provider_info["authorization_endpoint"] + "?" + query
+    print("Ask browser to authorize via", url)
+    return redirect(url)
+
+
+@app.route(_OID_CALLBACK_PATH)
+def oid_callback():
+    global provider_info
+    global client_id
+
+    # fetch stored state for this session needed to complete the handshake
+    auth_code = request.args["code"]
+    state = request.args["state"]
+    value = get_from_session_storage(state)
+
+    result, keypair = handle_callback(
+        value, provider_info, client_id, auth_code, _CALLBACK_URL
+    )
+
+    # if all went well, we have obtained a DPoP token
+    # set those as cookies
+    key, atoken = keypair.export(), result["access_token"]
+    session["key"] = key
+    session["access_token"] = atoken
+    # and give it to worker
+    headers, web_id = dpop_from_atoken_for_url(key, atoken, _TEST_URL, method="GET")
+    worker_state["users"][web_id] = headers
+    return redirect(value.pop("redirect_url"))
+
+
 @app.route("/")
 def hello_world():
-    global w
-    if w is None:
-        w = Thread(target=worker)
-        w.start()
+    global worker_thread
+    if worker_thread is None:
+        worker_thread = Thread(target=worker)
+        worker_thread.start()
 
-    msg = f"{datetime.now()}: Worker cycles: {state['worker']} latest: {state['latest']} worker alive: {w.is_alive()}"
+    msg = f"{datetime.now()}: Worker cycles: {worker_state['worker']} latest: {worker_state['latest']} worker alive: {worker_thread.is_alive()}"
     print(msg)
     return msg
-
---
+++ oidcutils.py
@@ -0,0 +1,122 @@
+import requests
+
+import jwcrypto
+import jwcrypto.jwk
+import jwcrypto.jws
+import jwcrypto.jwt
+
+from oic.oic import Client as OicClient
+from oic.utils.authn.client import CLIENT_AUTHN_METHOD
+
+import base64
+import datetime
+import json
+import hashlib
+import uuid
+from urllib.parse import urlencode
+
+def webid_from_access_token(access_token):
+    decoded_access_token = jwcrypto.jwt.JWT(jwt=access_token)
+    return json.loads(decoded_access_token.token.objects["payload"])["sub"]
+
+def dpop_from_atoken_for_url(key, access_token, url, method="GET"):
+    keypair = jwcrypto.jwk.JWK.from_json(key)
+
+    # We need to convert the access_token to DPoP to attach to our requsts.
+    # This is per URI and method!
+    headers = {
+        "Authorization": ("DPoP " + access_token),
+        "DPoP": _make_token_for(keypair, url, method),
+    }
+    return headers, webid_from_access_token(access_token)
+
+
+def prepare_auth_data(redirect_url, client_id, callback_url):
+    # create the data relevant to this session
+    # that shall be used when we come back from auth round trip
+    code_verifier, code_challenge = _make_verifier_challenge()
+    key = _make_random_string()
+    value = {"code_verifier": code_verifier, "redirect_url": redirect_url}
+
+    # now make the browser go to auth page and direct it to come back here (to cb route)
+    query = urlencode(
+        {
+            "code_challenge": code_challenge,
+            "state": key,
+            "response_type": "code",
+            "redirect_uri": callback_url,
+            "code_challenge_method": "S256",
+            "client_id": client_id,
+            # offline_access: also asks for refresh token
+            "scope": "openid offline_access",
+        }
+    )
+    return key, value, query
+
+
+def handle_callback(value, provider_info, client_id, auth_code, callback_url):
+    # Generate a key-pair.
+    keypair = jwcrypto.jwk.JWK.generate(kty="EC", crv="P-256")
+    code_verifier = value.pop("code_verifier")
+
+    # Exchange auth code for access token
+    resp = requests.post(
+        url=provider_info["token_endpoint"],
+        data={
+            "grant_type": "authorization_code",
+            "client_id": client_id,
+            "redirect_uri": callback_url,
+            "code": auth_code,
+            "code_verifier": code_verifier,
+        },
+        headers={
+            "DPoP": _make_token_for(keypair, provider_info["token_endpoint"], "POST")
+        },
+        allow_redirects=False,
+    )
+    return resp.json(), keypair
+
+
+def init_oidc(issuer, callback_url):
+    # https://pyoidc.readthedocs.io/en/latest/examples/rp.html#provider-info-discovery
+    provider_info = requests.get(issuer + ".well-known/openid-configuration").json()
+    client_id = _register_client(provider_info, callback_url)
+    print("oic client-id:", client_id)
+    return provider_info, client_id
+
+
+def _make_random_string():
+    return str(uuid.uuid4())
+
+def _make_verifier_challenge():
+    code_verifier = _make_random_string()
+    code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+    code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
+    code_challenge = code_challenge.replace("=", "")
+    return code_verifier, code_challenge
+
+
+def _register_client(provider_info, redirect):
+    # https://pyoidc.readthedocs.io/en/latest/examples/rp.html#client-registration
+    registration_response = OicClient(client_authn_method=CLIENT_AUTHN_METHOD).register(
+        provider_info["registration_endpoint"], redirect_uris=[redirect]
+    )
+    return registration_response["client_id"]
+
+
+def _make_token_for(keypair, uri, method):
+    jwt = jwcrypto.jwt.JWT(
+        header={
+            "typ": "dpop+jwt",
+            "alg": "ES256",
+            "jwk": keypair.export(private_key=False, as_dict=True),
+        },
+        claims={
+            "jti": _make_random_string(),
+            "htm": method,
+            "htu": uri,
+            "iat": int(datetime.datetime.now().timestamp()),
+        },
+    )
+    jwt.make_signed_token(keypair)
+    return jwt.serialize()
